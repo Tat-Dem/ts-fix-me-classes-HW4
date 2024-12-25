cab207c622f5d6ad6ea87fa94245b88a780cd3ce
Tat-Dem <innet06@gmail.com>
2024-12-25 22:05:16 +0200

    Home4 | Fixed constructor parameter type and implemented the deposit method correctly in bank-account.ts.

--- src/bank-account.ts
+++ src/bank-account.ts
@@ -1,15 +1,25 @@
 class BankAccount {
     balance: number
 
-    constructor(initialAmount: string) {
-        //balance = initialAmount
+    constructor(initialAmount: number) {
+        this.balance = initialAmount;
     }
 
     deposit(amount: number): number {
-        this.balance
-
+        this.balance += amount;
+        return this.balance;
+    }
+    withdraw (amount: number): number{
+        if (this.balance >= amount){
+            this.balance -= amount;
+        } else {
+            console.log('Insufficient funds.');
+        }
+        return this.balance;
     }
 }
 
-const myAccount = new BankAccount(500)
-console.log(myAccount.deposit(100)) // After fixing: will print 600
+const myAccount = new BankAccount(500);
+console.log(myAccount.deposit(100)); // After fixing: will print 600
+console.log(myAccount.withdraw(200)); // After fixing: will print 400
+console.log(myAccount.withdraw(500)); // After fixing: will print 'Insufficient funds.'
