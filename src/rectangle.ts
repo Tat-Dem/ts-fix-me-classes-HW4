c6caf8fdf2a566fa891e2fd8ae03cbfd0a82d252
Tat-Dem <innet06@gmail.com>
2024-12-25 22:18:31 +0200

    Home4 | In the file rectangle.ts.ts,the correct initialization of the height and the calculation of the area in the getArea method have been ensured.

--- src/rectangle.ts
+++ src/rectangle.ts
@@ -1,14 +1,14 @@
 class Rectangle {
-    width: number
-    height: number
+    width: number;
+    height: number;
 
     constructor(width: number, height: number) {
-        this.Width = width
-        this.Height = height
+        this.width = width;
+        this.height = height;
     }
 
     getArea(): number {
-        return width * height
+        return this.width * this.height;
     }
 }
 
